afae070a4f26b57d941c2b23f13d66b455c3d9b5
kedir m <feedonat@gmail.com>
2024-02-17 10:30:47 -0800

    added drower and groups

--- src/app/pages/tabs/tab1/tab1.page.ts
+++ src/app/pages/tabs/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
+  categories = [];
   constructor() {}
+  ngOnInit(): void {
+    
+    this.categories.push('Rental')
+    this.categories.push('Buy&Sell')
+    this.categories.push('Daycare')
+    this.categories.push('Asbeza')
+    this.categories.push('Transfer')
+    this.categories.push('Car')
+    this.categories.push('Ziwaj')
+  }
 
 }
