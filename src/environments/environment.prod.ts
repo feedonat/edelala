5a4066b45a31784d4673ed18a2a0b20fa464a025
Kedir Yibrie <feedonat@gmail.com>
2024-02-20 22:23:49 -0600

    Update environment.prod.ts
    prod db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase  : {
+    apiKey: "AIzaSyAq5VK6G-YRq5bCgIVufrrwi0y1VLxICjQ",
+    authDomain: "edelala.firebaseapp.com",
+    projectId: "edelala",
+    storageBucket: "edelala.appspot.com",
+    messagingSenderId: "555833315107",
+    appId: "1:555833315107:web:ca9b021eaaf0baff848696",
+    measurementId: "G-Z7C0NQPBWJ"
+  }
 };
